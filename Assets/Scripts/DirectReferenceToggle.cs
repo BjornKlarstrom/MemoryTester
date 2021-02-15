eb414e3657b89eebc5969c3abd170bbb0a49e2d8
BjornKlarstrom <bjorn.klarstrom@gmail.com>
2021-02-15 12:24:54 +0100

    adds test for 1,2,3 assignment

---
+++ Assets/Scripts/DirectReferenceToggle.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectReferenceToggle : MonoBehaviour
+{
+    public Material skybox0;
+    public Material skybox1;
+    public Material skybox2;
+    public Material skybox3;
+    public Material skybox4;
+    public Material skybox5;
+    
+    
+}
